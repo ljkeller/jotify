5744bb25d4eb9842095ce401346fd5da11ea6582
ljkeller <kellerjlucas@gmail.com>
2024-01-02 19:38:06 -0600

    Next JS migration
    Migrate to nextjs. Routing out of app folder.
    
    Have some tutorial residue still, but I'm learning as I'm going.

---
+++ app/page.jsx
@@ -0,0 +1,21 @@
+import LikeButton from './like-button';
+ 
+function Header({ title }) {
+  return <h1>{title ? title : 'Default title'}</h1>;
+}
+ 
+export default function HomePage() {
+  const names = ['Ada Lovelace', 'Grace Hopper', 'Margaret Hamilton'];
+ 
+  return (
+    <div>
+      <Header title="Jotify.io, the Scott County inmate library" />
+      <ul>
+        {names.map((name) => (
+          <li key={name}>{name}</li>
+        ))}
+      </ul>
+      <LikeButton />
+    </div>
+  );
+}

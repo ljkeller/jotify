397e16431b4dc85101fd9a2b5e9d8b6e750b40e2
ljkeller <kellerjlucas@gmail.com>
2024-01-02 23:02:37 -0600

    Naive SSR with sqlite3

--- app/page.jsx
+++ app/page.jsx
@@ -1,20 +1,29 @@
+import Database from 'better-sqlite3';
+
 import LikeButton from './like-button';
- 
+import Inmate from '../tools/models/Inmate';
+import { tables } from '../tools/config';
+
 function Header({ title }) {
   return <h1>{title ? title : 'Default title'}</h1>;
 }
- 
+
 export default function HomePage() {
-  const names = ['Ada Lovelace', 'Grace Hopper', 'Margaret Hamilton'];
- 
+  const db = new Database('jotify.db', { verbose: console.log, readonly: true });
+
+  const inmateQuery = db.prepare(`SELECT id, first_name, middle_name, last_name, age, booking_date, arresting_agency, charges, url FROM ${tables.inmates} LIMIT 10;`).all();
+  const inmates = inmateQuery.map((inmate) => ({ id: inmate.id, inmate: new Inmate(inmate.first_name, inmate.middle_name, inmate.last_name, inmate.age, inmate.booking_date, inmate.arresting_agency, inmate.charges, null, inmate.url, []) }));
+
+  db.close();
+
   return (
     <div>
       <Header title="Jotify.io, the Scott County inmate library" />
-      <ul>
-        {names.map((name) => (
-          <li key={name}>{name}</li>
+      <ol>
+        {inmates.map((inmate_id_pair) => (
+          <li key={inmate_id_pair.id}>{JSON.stringify(inmate_id_pair.inmate)}</li>
         ))}
-      </ul>
+      </ol>
       <LikeButton />
     </div>
   );

ca6aa5e36a60aef66b53d2ad49c8193cb9985f5e
ljkeller <kellerjlucas@gmail.com>
2024-01-03 22:38:13 -0600

    Fix title, desc, cleanup
    Forgot that my layout.js was generated. That is where my metadata will
    go.

--- app/page.jsx
+++ app/page.jsx
@@ -1,20 +1,13 @@
 import Database from 'better-sqlite3';
 
-import LikeButton from './like-button';
 import Inmate from '../tools/models/Inmate';
 import { tables } from '../tools/config';
 
-// pages/index.js
-import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <Head>
-        <title>Jotify.io</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
@@ -27,12 +20,10 @@ export default function Home() {
         </div>
 
         <div className={styles.icons}>
-          {/* Place your icons here */}
         </div>
       </main>
 
       <footer className={styles.footer}>
-        {/* Footer content */}
       </footer>
     </div>
   )

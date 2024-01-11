feadf59ab08df81406833bfcc7c759fdd57d776a
ljkeller <kellerjlucas@gmail.com>
2024-01-10 23:07:21 -0600

    Redefined database schema
    New models that are here to stay. Except for img table, that should be
    replaced with cloud service like s3.

--- tools/config.js
+++ tools/config.js
@@ -8,7 +8,7 @@ const config = {
   baseInmateLink: "https://www.scottcountyiowa.us/sheriff/inmates.php?",
 
   sleepBetweenRequests: 125, // ms
-  databaseFile: ":memory:",
+  databaseFile: "scjailio-tmp.db",
   lastNDays: 1,
 };
 
@@ -52,16 +52,63 @@ const tableCreate = {
 
 const scJailIoTableCreate = {
   inmate: `
+    CREATE TABLE IF NOT EXISTS inmate (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      first_name TEXT NOT NULL CHECK (first_name <> ''),
+      middle_name TEXT,
+      last_name TEXT NOT NULL CHECK (last_name <> ''),
+      affix TEXT,
+      permanent_id TEXT,
+      sex TEXT,
+      dob TEXT NOT NULL CHECK (dob <> ''),
+      height TEXT,
+      weight TEXT,
+      race TEXT,
+      eye_color TEXT,
+      img_url TEXT
+    )
   `,
   alias: `
+    CREATE TABLE IF NOT EXISTS alias (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      alias TEXT UNIQUE NOT NULL CHECK (alias <> '')
+    )
   `,
   inmateAliasJunction: `
+    CREATE TABLE IF NOT EXISTS inmate_alias (
+      inmate_id INTEGER NOT NULL,
+      alias_id INTEGER NOT NULL,
+      FOREIGN KEY (inmate_id) REFERENCES inmate(id),
+      FOREIGN KEY (alias_id) REFERENCES aliases(id),
+      PRIMARY KEY (inmate_id, alias_id)
+    )
   `,
   img: `
+    CREATE TABLE IF NOT EXISTS img (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      inmate_id INTEGER NOT NULL,
+      img BLOB,
+      FOREIGN KEY (inmate_id) REFERENCES inmate(id)
+    )
   `,
   bondInformation: `
+    CREATE TABLE IF NOT EXISTS bond (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      inmate_id INTEGER NOT NULL,
+      type TEXT NOT NULL CHECK (type <> ''),
+      amount_pennies INTEGER NOT NULL,
+      FOREIGN KEY (inmate_id) REFERENCES inmate(id)
+    )
   `,
   chargeInformation: `
+    CREATE TABLE IF NOT EXISTS charge (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      inmate_id INTEGER NOT NULL,
+      description TEXT NOT NULL CHECK (description <> ''),
+      grade TEXT NOT NULL CHECK (grade <> ''),
+      offense_date TEXT NOT NULL CHECK (offense_date <> ''),
+      FOREIGN KEY (inmate_id) REFERENCES inmate(id)
+    )
   `
 };
 
--- tools/recordCrawler.js
+++ tools/recordCrawler.js
@@ -2,47 +2,47 @@
 
 const Database = require('better-sqlite3');
 
-const { config, tables, scJailIoTableCreate } = require("./config");
+const { config, scJailIoTableCreate } = require("./config");
 const { getLastNDaysLocal } = require("./dateUtils");
 const { getListings } = require("./scraping/inmateScraper");
 
 main();
 
 async function main() {
-    const dates = getLastNDaysLocal(config.lastNDays);
-    // eventually, we will want to crawl continuously
+  const dates = getLastNDaysLocal(config.lastNDays);
+  // eventually, we will want to crawl continuously
 
-    const db = new Database(config.databaseFile, { verbose: console.log });
-    setupDbCloseConditions(db);
+  const db = new Database(config.databaseFile, { verbose: console.log });
+  setupDbCloseConditions(db);
 
-    try {
-        createTables(db);
-        const listings = await getListings(dates);
+  try {
+    createTables(db);
+    const listings = await getListings(dates);
 
-        for (const listing of listings) {
-        }
-    } catch (err) {
-        console.log(err);
-    } finally {
-        db.close();
-        console.log("Finished building db.");
+    for (const listing of listings) {
     }
+  } catch (err) {
+    console.log(err);
+  } finally {
+    db.close();
+    console.log("Finished building db.");
+  }
 }
 
 function setupDbCloseConditions(db) {
-    process.on("exit", () => db.close());
-    process.on("SIGHUP", () => process.exit(128 + 1));
-    process.on("SIGINT", () => process.exit(128 + 2));
-    process.on("SIGTERM", () => process.exit(128 + 15));
+  process.on("exit", () => db.close());
+  process.on("SIGHUP", () => process.exit(128 + 1));
+  process.on("SIGINT", () => process.exit(128 + 2));
+  process.on("SIGTERM", () => process.exit(128 + 15));
 }
 
 function createTables(db) {
-    db.prepare(scJailIoTableCreate.inmate).run();
-    db.prepare(scJailIoTableCreate.alias).run();
-    db.prepare(scJailIoTableCreate.inmateAliasJunction).run();
-    // Eventually, we should store images in S3
-    // For now, we need to archive
-    db.prepare(scJailIoTableCreate.img).run();
-    db.prepare(scJailIoTableCreate.bondInformation).run();
-    db.prepare(scJailIoTableCreate.chargeInformation).run();
+  db.prepare(scJailIoTableCreate.inmate).run();
+  db.prepare(scJailIoTableCreate.alias).run();
+  db.prepare(scJailIoTableCreate.inmateAliasJunction).run();
+  // Eventually, we should store images in S3
+  // For now, we need to archive
+  db.prepare(scJailIoTableCreate.img).run();
+  db.prepare(scJailIoTableCreate.bondInformation).run();
+  db.prepare(scJailIoTableCreate.chargeInformation).run();
 }

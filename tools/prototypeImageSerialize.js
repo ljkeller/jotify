b12a186f8dd68b61f32e9b051668d61e5828f4c6
ljkeller <kellerjlucas@gmail.com>
2024-01-20 14:21:03 -0600

    Image serialization prototype
    Previous img logic was doing using utf8 at request time, dropping
    essential jpeg information. As a consequence, all db entries up until
    now will have invalid imgs.
    
    Wrote a proof of concept for what I need to implement.
    
    Should write a patch script that fixes the last 7 days of data.

---
+++ tools/prototypeImageSerialize.js
@@ -0,0 +1,36 @@
+// Make a prototype for retrieving an image, serializing it, storing it, and then displaying it
+const axios = require('axios');
+const Database = require('better-sqlite3');
+const fs = require('fs');
+
+const url = 'https://www3.scottcountyiowa.gov/sheriff/images/inmates/215417.jpg';
+async function main() {
+  const resp = await axios.get(url, { responseType: 'arraybuffer', responseEncoding: 'binary' });
+  console.log(resp);
+
+  fs.writeFileSync('testPreSerialize.jpg', resp.data, 'binary');
+  const db = new Database(':memory:', { verbose: console.log });
+  db.prepare(`
+    CREATE TABLE IF NOT EXISTS img (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      img BLOB
+    )
+  `).run();
+
+  db.prepare(`
+    INSERT INTO img
+    VALUES
+    (null, @img)
+  `).run({ img: resp.data });
+
+  const postSerialize = db.prepare(`
+    SELECT img
+    FROM img
+    WHERE id = 1`).get();
+  console.log(postSerialize);
+  fs.writeFileSync('testPostSerialize.jpg', postSerialize.img, 'binary');
+
+  db.close();
+}
+
+main();

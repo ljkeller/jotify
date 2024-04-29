121f85e9ce4c20793b3d6203c024f87a3abb365d
ljkeller <kellerjlucas@gmail.com>
2024-04-29 06:31:25 -0600

    Cleanup imports / format

--- app/page.jsx
+++ app/page.jsx
@@ -1,33 +1,29 @@
-import { RiUserSearchFill } from "react-icons/ri";
-import { FaMask } from 'react-icons/fa';
+import styles from "../styles/Home.module.css";
+import { formatISO } from "date-fns";
 
-import styles from '../styles/Home.module.css'
-import Database from 'better-sqlite3';
-import { formatISO } from 'date-fns';
+import TrafficCalendar from "./ui/trafficCalendar";
+import Search from "/app/ui/search";
+import Record from "/app/ui/compressedRecord";
+import { runtimeDbConfig } from "/tools/config";
 
-import TrafficCalendar from './ui/trafficCalendar';
-import Search from '/app/ui/search';
-import Record from '/app/ui/compressedRecord';
-import { runtimeDbConfig, DBConfig } from '/tools/config';
-
-import SqlControllerFactory from '/tools/database/sqlControllerFactory';
+import SqlControllerFactory from "/tools/database/sqlControllerFactory";
 
 async function getLast7DaysInmateTraffic(sqlController) {
   const traffic = [];
   for (let i = 6; i >= 0; i--) {
     let date = new Date();
     date.setDate(date.getDate() - i);
-    const dateStr = formatISO(date, { representation: 'date' });
+    const dateStr = formatISO(date, { representation: "date" });
     const count = await sqlController.countInmatesOnDate(dateStr);
-    traffic.push({ date: dateStr, inmateCount: count});
+    traffic.push({ date: dateStr, inmateCount: count });
   }
   return traffic;
 }
 
 export const metadata = {
-  title: 'scjail.io Home',
-  description: 'Scott county inmate listing- but better',
-}
+  title: "scjail.io Home",
+  description: "Scott county inmate listing- but better",
+};
 
 export default async function Home() {
   // TODO: Create singletons for writable sqlite, readonly sqlite, and postgres? Then return the singleton from the factory?
@@ -36,14 +32,16 @@ export default async function Home() {
 
   const trafficLast7Days = await getLast7DaysInmateTraffic(sqlController);
 
-  const compressedRecordInfo = await sqlController.getCompressedInmateDataForDate(formatISO(new Date(), { representation: 'date' }));
-  const compressedRecords = compressedRecordInfo.map((inmate, idx) =>
+  const compressedRecordInfo =
+    await sqlController.getCompressedInmateDataForDate(
+      formatISO(new Date(), { representation: "date" })
+    );
+  const compressedRecords = compressedRecordInfo.map((inmate, idx) => (
     <Record key={idx} data={inmate} priority={idx < 5} />
-  );
+  ));
 
   return (
     <div className={styles.container}>
-
       <main className={styles.main}>
         <h1 className={styles.title}>
           <span className={styles.complementary}>sc</span>
@@ -56,10 +54,8 @@ export default async function Home() {
           <TrafficCalendar inmateTraffic7Days={trafficLast7Days} />
         </div>
 
-        <div className={styles.records}>
-          {compressedRecords}
-        </div>
-      </main >
-    </div >
-  )
+        <div className={styles.records}>{compressedRecords}</div>
+      </main>
+    </div>
+  );
 }
